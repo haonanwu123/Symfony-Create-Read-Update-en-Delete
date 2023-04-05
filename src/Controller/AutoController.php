b65803a96e86c831f44893bf44cd169a3df8bd07
haonanwu123 <wuhaonan99@gmail.com>
2023-04-05 14:52:45 +0200

    afmaken

--- src/Controller/AutoController.php
+++ src/Controller/AutoController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Autos;
+use App\Form\InsertType;
 use App\Repository\AutosRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,10 +33,10 @@ class AutoController extends AbstractController
     }
 
     #[Route('/update/{id}', name:'app_update')]
-    public function update(AutosRepository $ar, Request $request, Autos $car , EntityManagerInterface $entityManager): Response
+    public function update(AutosRepository $cars, Request $request, Autos $car , EntityManagerInterface $entityManager): Response
     {
         $autoId = $car->getId();
-        $auto = $ar->find($autoId);
+        $auto = $cars->find($autoId);
         $form = $this->createForm(UpdateType::class, $auto);
 
         $form->handleRequest($request);
@@ -45,11 +46,53 @@ class AutoController extends AbstractController
             $entityManager->persist($add);
             $entityManager->flush();
 
-            $this->addFlash('warning', 'Rij gewijzigd');
+            $this->addFlash('warning', 'Rij toegevoegd');
             return $this->redirectToRoute('app_home');
         }
 
         return $this->renderForm('bezoeker/update.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/delete/{id}', name:'app_delete')]
+    public function delete(Autos $delete, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $deleteAuto = $delete->getModel();
+        $deleteAutos = $entityManager->getRepository(Autos::class)->find($id);
+
+        if (!$deleteAutos) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $entityManager->remove($deleteAutos);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home',[
+            'deleteAuto'=> $deleteAuto
+        ]);
+    }
+
+    #[Route('/insert', name:'app_insert')]
+    public function order(AutosRepository $autosRepository,Request $request): Response
+    {
+
+        $insert = new Autos();
+
+        $form = $this->createForm(InsertType::class, $insert);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $insert = $form->getData();
+            $autosRepository->save($insert);
+
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->renderForm('bezoeker/insert.html.twig', [
             'form' => $form,
         ]);
     }
---
+++ src/Form/InsertType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Autos;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InsertType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('model',TextType::class)
+            ->add('type',TextType::class)
+            ->add('kleur',TextType::class)
+            ->add('gewicht',TextType::class)
+            ->add('prijs',TextType::class)
+            ->add('voorraad',TextType::class)
+            ->add('insert',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Autos::class,
+        ]);
+    }
+}
